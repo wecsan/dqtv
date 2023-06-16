2a7cf8bd90008a384d83fae4436dec95a4e8e446
wecsyu <74102486+wecsan@users.noreply.github.com>
2023-06-16 19:38:07 +0800

    Add files via upload

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -55,16 +55,18 @@ public class App extends MultiDexApplication {
     }
 
     private void initParams() {
-
-    putDefault(HawkConfig.HOME_REC, 1);       // Home Rec 0=豆瓣, 1=推荐, 2=历史
-    putDefault(HawkConfig.PLAY_TYPE, 1);      // Player   0=系统, 1=IJK, 2=Exo
-    putDefault(HawkConfig.IJK_CODEC, "软解码");// IJK Render 软解码, 硬解码
-    putDefault(HawkConfig.HOME_SHOW_SOURCE, true);  // true=Show, false=Not show
-    putDefault(HawkConfig.HOME_NUM, 2);       // History Number
-    putDefault(HawkConfig.DOH_URL, 2);        // DNS
-    putDefault(HawkConfig.SEARCH_VIEW, 2);    // Text or Picture
-
-}
+        // Hawk
+        Hawk.init(this).build();
+        Hawk.put(HawkConfig.DEBUG_OPEN, false);
+        if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {
+           Hawk.put(HawkConfig.HOME_REC, 2);       // Home Rec 0=豆瓣, 1=推荐, 2=历史
+    Hawk.put(HawkConfig.PLAY_TYPE, 1);      // Player   0=系统, 1=IJK, 2=Exo
+    Hawk.put(HawkConfig.IJK_CODEC, "软解码");// IJK Render 软解码, 硬解码
+    Hawk.put(HawkConfig.HOME_SHOW_SOURCE, true);  // true=Show, false=Not show
+    Hawk.put(HawkConfig.HOME_NUM, 2);       // History Number
+    Hawk.put(HawkConfig.DOH_URL, 2);        // DNS
+    Hawk.put(HawkConfig.SEARCH_VIEW, 2);    // Text or Picture
+        }
     }
 
     public static App getInstance() {
